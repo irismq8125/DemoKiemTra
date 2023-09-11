e6bcdc72b5062fd76093407da3dcd9b8640f403c
hv <hv@PM304-GV>
2023-09-11 21:04:01 +0700

    Add project files.

---
+++ DemoKiemTra/Controllers/DanhSachController.cs
@@ -0,0 +1,36 @@
+﻿using DemoKiemTra.Models.DanhSach;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoKiemTra.Controllers
+{
+    public class DanhSachController : Controller
+    {
+        public static List<ModelDanhSach> products = new List<ModelDanhSach>();
+        public IActionResult Index(bool? check = false)
+        {
+            if ((bool)check)
+            {
+                products.Clear();
+            }
+            else
+            {
+                return View(products.ToList());
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string tensanpham, int soluong, int dongia)
+        {
+            var homeProduct = new ModelDanhSach(tensanpham, soluong, dongia);
+            products.Add(homeProduct);
+            return View(products);
+        }
+
+        public IActionResult RemoveProduct(int position)
+        {
+            products.RemoveAt(position - 1);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ DemoKiemTra/Models/DanhSach/ModelDanhSach.cs
@@ -0,0 +1,18 @@
+﻿namespace DemoKiemTra.Models.DanhSach
+{
+    public class ModelDanhSach
+    {
+        public string TenSanPham { get; set; }
+        public int SoLuong { get; set; }
+        public int DonGia { get; set; }
+        public int TamTinh { get; set; }
+        public ModelDanhSach() { }
+        public ModelDanhSach(string tenSanPham, int soLuong, int gia)
+        {
+            TenSanPham = tenSanPham;
+            SoLuong = soLuong;
+            DonGia = gia;
+            TamTinh = soLuong * gia;
+        }
+    }
+}
